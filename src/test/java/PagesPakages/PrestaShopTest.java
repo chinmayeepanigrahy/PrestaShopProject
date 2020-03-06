5205483bd1f5efcc77a735f7200054bf1472c8a7
chinmayeepanigrahy <55028727+chinmayeepanigrahy@users.noreply.github.com>
2020-03-06 09:27:50 +0100

    Hi

--- src/test/java/PagesPakages/PrestaShopTest.java
+++ src/test/java/PagesPakages/PrestaShopTest.java
@@ -1,24 +1,23 @@
 package PagesPakages;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 import java.util.concurrent.TimeUnit;
 
 public class PrestaShopTest {
     static WebDriver driver = new ChromeDriver();
 
-
     @BeforeClass
     public static void setUp() {
         System.out.println("Beforeclass ..... setUp");
         driver.get("http://demo.prestashop.com/#/en/front");
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //  driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
     }
 
     @Before
@@ -27,26 +26,29 @@ public class PrestaShopTest {
         //click homebutton
     }
 
-    //As a user i want to click a log in account
+    //1.As a user i want to click a log in account
     @Test
     public void clickSignInButton() {
-        //driver.findElement(By.xpath("//iframe[@id='framelive']"));
         driver.switchTo().frame("framelive");
         driver.findElement(By.xpath("//a[@title='Log in to your customer account']")).click();
+        driver.navigate().back();
+        driver.navigate().refresh();
     }
 
-    //As a user i want to create a new account
+    //2.As a user i want to create a new account
     @Test
-    public void userStory3() {
+    public void userStory2() {
         driver.switchTo().frame("framelive");
         driver.findElement(By.xpath("//a[@title='Log in to your customer account']")).click();
         driver.findElement(By.cssSelector(".no-account")).click();
+        driver.navigate().back();
+        driver.navigate().refresh();
 
     }
-    // As a user i created a new account and out some details
+    //3. As a user i created a new account and out some details
 
     @Test
-    public void userStory4() {
+    public void userStory3() {
         driver.switchTo().frame("framelive");
         driver.findElement(By.xpath("//a[@title='Log in to your customer account']")).click();
         driver.findElement(By.cssSelector(".no-account")).click();
@@ -58,48 +60,68 @@ public class PrestaShopTest {
         driver.findElement(By.name("birthday")).sendKeys("07/20/1988");
         driver.findElement(By.name("psgdpr")).click();
         driver.findElement(By.cssSelector(".form-control-submit")).click();
-
-    }
-
-    @Test
-    public void userStory() {
-        driver.switchTo().frame("framelive");
-        driver.manage().window().maximize();
-        driver.findElement(By.id("category-9")).click();
-        driver.findElement(By.cssSelector(".ps-shown-by-js")).click();
-        driver.findElement(By.linkText("Today is a good day Framed...")).click();
+        driver.navigate().back();
+        driver.navigate().back();
+        driver.navigate().refresh();
 
     }
 
     //As a user i want to search Accessories
+
+
     @Test
     public void userStory5() {
-
-        driver.switchTo().frame("framelive");
-        driver.findElement(By.name("s")).sendKeys("Accessories");
-        driver.findElement(By.xpath("//input[@name='s']")).click();
+        try {
+            driver.switchTo().frame("framelive");
+            driver.findElement(By.id("category-6")).click();
+            driver.findElement(By.linkText("Brown bear cushion")).click();
+           driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+           driver.findElement(By.xpath("//input[@name='s']")).click();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    @Test
-    public void userStory7() {
 
+    //As a user user i am taking contact us as a user story
+
+    @Test
+    public void userStory6() {
         driver.switchTo().frame("framelive");
-        driver.manage().window().maximize();
         driver.findElement(By.linkText("Contact us")).click();
         driver.findElement(By.name("id_contact")).click();
         driver.findElement(By.name("from")).sendKeys("chinu.pani@gmail.com");
-        driver.findElement(By.cssSelector(".buttonText")).sendKeys("c://newhtml.html");
         driver.findElement(By.name("message")).sendKeys("Hello i want to change mt dress");
         driver.findElement(By.name("submitMessage")).click();
+        driver.navigate().back();
+        driver.navigate().back();
+        driver.navigate().refresh();
     }
 
+    //AS a user i want to subscribe for a newsletter
+    @Test
+    public void userStory7() {
+        driver.switchTo().frame("framelive");
+        driver.findElement(By.name("email")).sendKeys("chuni.padhy@gmail.com");
+        driver.findElement(By.name("submitNewsletter")).click();
+        driver.navigate().back();
+        driver.navigate().refresh();
+
+    }
+
+    //As a user i want to search a Art size of (40*60)
     @Test
     public void userStory8() {
 
-        driver.switchTo().frame("framelive");
-        driver.manage().window().maximize();
-        driver.findElement(By.name("submitNewsletter")).sendKeys("shreeya.panigrahy@gmail.com");
+        try {
+            driver.switchTo().frame("framelive");
+            driver.findElement(By.id("category-9")).click();
+            driver.findElement(By.cssSelector(".ps-shown-by-js")).click();
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
+
 }
--- src/test/java/PagesPakages/TestRunnerMain.java
+++ src/test/java/PagesPakages/TestRunnerMain.java
@@ -6,6 +6,8 @@ import org.junit.runner.Result;
 
 public class TestRunnerMain {
     public static void main(String[] args)  {
+       // System.setProperty("Webdriver.chrome.driver","‪C:\\Users\\chinm\\WORKSPACESELENIUM\\PrestashopSeleniumJunit\\chromedriver.exe");
+
         Result result = JUnitCore.runClasses(PrestaShopTest.class);
         System.out.println("Result =" + result.wasSuccessful());
 
